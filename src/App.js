60eeab07921121f2c2c8b12abb231d8fea2b44b9
plondon <philip@blockchain.com>
2018-09-22 16:06:24 -0400

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import styled, { injectGlobal } from 'styled-components'
+import Auth from './scenes/Auth'
+import Login from './scenes/Login'
+import Dashboard from './scenes/Dashboard'
+import Slideshow from './scenes/Slideshow'
 
-import Auth from "./scenes/Auth";
-import Login from "./scenes/Login";
-import Dashboard from "./scenes/Dashboard";
-import Slideshow from "./scenes/Slideshow";
+injectGlobal`
+  html,
+  body,
+  #root {
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  .slick-slider {
+    text-align: center;
+  }
+  .slick-slide img {
+    height: 100vh;
+    width: auto !important;
+  }
+`
+
+const Wrapper = styled.div`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 100%;
+`
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <Switch>
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/slideshow" component={Slideshow} />
-        <Route path="/auth" component={Auth} />
-        <Route component={Login} />
-      </Switch>
-    );
+      <Wrapper>
+        <Switch>
+          <Route path='/dashboard' component={Dashboard} />
+          <Route path='/slideshow' component={Slideshow} />
+          <Route path='/auth' component={Auth} />
+          <Route component={Login} />
+        </Switch>
+      </Wrapper>
+    )
   }
 }
 
-export default App;
+export default App
--- src/scenes/Login/index.js
+++ src/scenes/Login/index.js
@@ -1,22 +1,35 @@
-import React, { Component } from "react";
-import * as Dropbox from "dropbox";
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import * as Dropbox from 'dropbox'
 
-const dbx = new Dropbox.Dropbox({ clientId: "koee38ql2uh6axd" });
+const dbx = new Dropbox.Dropbox({ clientId: 'koee38ql2uh6axd' })
+
+const Wrapper = styled.div`
+  * {
+    color: white
+  }
+  text-align: center;
+`
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     const authUrl = dbx.getAuthenticationUrl(
-      "https://dbox-slideshow.firebaseapp.com/auth/"
-    );
+      'http://localhost:3000/auth/'
+    )
     this.state = {
       authUrl
-    };
+    }
   }
 
-  render() {
-    const { authUrl } = this.state;
-    return <a href={authUrl}>Log In</a>;
+  render () {
+    const { authUrl } = this.state
+    return (
+      <Wrapper>
+        <h1>Welcome to Dropbox Slideshow</h1>
+        <a href={authUrl}>Click Here to Log In</a>
+      </Wrapper>
+    )
   }
 }
-export default Login;
+export default Login
